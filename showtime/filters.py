3ce386faa2115c7d8acec99f27ca62af10cb2787
Sagar Neupane <cyber.crime252@gmail.com>
2022-09-21 18:22:11 +0545

    added four filter fields -hour, day, month, year

--- showtime/filters.py
+++ showtime/filters.py
@@ -1,21 +1,24 @@
-from datetime import date
+from datetime import datetime
 from django_filters import rest_framework as filter
 
 from showtime.models import ShowTime
 
 
 class MyDateFilter(filter.FilterSet):
-
-    filter_date = filter.DateTimeFilter(field_name="show_start_time", lookup_expr="date")
+    year = filter.NumberFilter(field_name="show_start_time", lookup_expr="year")
+    month = filter.NumberFilter(field_name="show_start_time", lookup_expr="month")
+    date = filter.NumberFilter(field_name="show_start_time", lookup_expr="day")
+    hour = filter.NumberFilter(field_name="show_start_time", lookup_expr="hour")
 
     class Meta:
         model = ShowTime
         fields = ["show_start_time",]
 
     def __init__(self, data, *args, **kwargs):
-        if not data.get('filter_date'):
+        if not data.get('date'):
             data = data.copy()
-            data['filter_date'] = date.today()
+            data['date'] = datetime.now().strftime("%d")
+            # data['date'] = 22
         super().__init__(data, *args, **kwargs)
 
 
